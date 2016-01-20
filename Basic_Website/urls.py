511ffb7df985bb919ae3d816a378a0e5374e970e
nandopatel <nandopatel2001@yahoo.co.uk>
2016-01-20 19:44:25 +0000

    Fixed urls

--- Basic_Website/urls.py
+++ Basic_Website/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Basic_Website.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^$','Webapp.views.index'),
+	url(r'^$','Webapp.views.index',name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
