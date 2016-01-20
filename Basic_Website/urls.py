99c95df6b3f701c90eba9dcb082bd0e07ad0c6c1
nandopatel <nandopatel2001@yahoo.co.uk>
2016-01-20 19:54:29 +0000

    Fixed the views.py

--- Basic_Website/urls.py
+++ Basic_Website/urls.py
@@ -5,6 +5,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Basic_Website.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^$','Webapp.views.index',name='index'),
+	url(r'^$','Webapp.views.base',name='base'),
+	url(r'booking.html','Webapp.views.booking',name='booking'),
+	url(r'gallery.html','Webapp.views.gallery',name='gallery'),
+	url(r'currency','Webapp.views.currency',name='currency'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- Webapp/views.py
+++ Webapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-	return render(request,'index.html')
+def base(request):
+	return render(request,'base.html')
 
 def booking(request):
 	return render(request,'booking.html')
