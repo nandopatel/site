f0465637c478041ff87789e43fc0edb567517f43
nandopatel <nandopatel2001@yahoo.co.uk>
2016-01-20 19:37:29 +0000

    Fixed the views.py

--- Webapp/views.py
+++ Webapp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
+
+def booking(request):
+	return render(request,'booking.html')
+	
+def currency(request):
+	return render(request,'currency.html')
+
+def gallery(request):
+	return render(request,'gallery.html')
