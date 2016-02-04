cac567482810fed6b7237d96f900df66a2ceaf78
nandopatel <nandopatel2001@yahoo.co.uk>
2016-02-04 20:44:21 +0000

    test

--- Basic_Website/settings.py
+++ Basic_Website/settings.py
@@ -121,6 +121,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
                # If True, users can register
+SITE_ID = 1
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
 LOGIN_REDIRECT_URL = '/'  # The page you want users to arrive at after they successful log in
