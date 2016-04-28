c0f35aab0dab3336ddf276bd9fa03383f331b050
nandopatel <nandopatel2001@yahoo.co.uk>
2016-04-28 20:48:54 +0100

    'test'

--- Webapp/views.py
+++ Webapp/views.py
@@ -29,7 +29,7 @@ def formulas(request):
 	form = NameForm(request.POST)
 	
 	context= {
-	#'distance': form.data['distance'],
+	'distance': form.data['distance'],
 	#'timetaken': form.data['timetaken'],
 	#'mass': form.data['mass'],
 	#'gforce': form.data['gforce'],
