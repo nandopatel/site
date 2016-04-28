506038eb2575642f5244defd8a6511931e3d0377
nandopatel <nandopatel2001@yahoo.co.uk>
2016-04-28 20:42:14 +0100

    'test'

--- Webapp/forms.py
+++ Webapp/forms.py
@@ -4,7 +4,7 @@ class NameForm(forms.Form):
 	#your_name = forms.CharField(label='Name', max_length=100)
 	#your_move= forms.CharField(label='Move', max_length=100)
 	#yesorno= forms.CharField(label='YesNo', max_length=100)
-	distance=forms.CharField(label='Distance Travelled', max_length=100)
+	distance=forms.CharField(label='Distance Travelled', max_length=100,required=False)
 	timetaken=forms.CharField(label='Time taken', max_length=100)
 	mass=forms.CharField(label='Enter mass', max_length=100)
 	gforce=forms.CharField(label='Gravitational force:', max_length=100)
