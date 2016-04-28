767f52e5319d455ef4696b7f50096d9abb00433c
nandopatel <nandopatel2001@yahoo.co.uk>
2016-04-28 20:40:01 +0100

    'test'

--- Webapp/forms.py
+++ Webapp/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class NameForm(forms.Form):
-	your_name = forms.CharField(label='Name', max_length=100)
-	your_move= forms.CharField(label='Move', max_length=100)
-	yesorno= forms.CharField(label='YesNo', max_length=100)
+	#your_name = forms.CharField(label='Name', max_length=100)
+	#your_move= forms.CharField(label='Move', max_length=100)
+	#yesorno= forms.CharField(label='YesNo', max_length=100)
 	distance=forms.CharField(label='Distance Travelled', max_length=100)
 	timetaken=forms.CharField(label='Time taken', max_length=100)
 	mass=forms.CharField(label='Enter mass', max_length=100)
