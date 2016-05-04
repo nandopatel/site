cb757eaf0bfc053e96c71eaea192c783c654e961
nandopatel <nandopatel2001@yahoo.co.uk>
2016-05-04 20:43:21 +0100

    'test'

--- Webapp/models.py
+++ Webapp/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class speedandweight(models.Model):
 	comments=models.IntegerField(blank=True)
 	weight=models.IntegerField(blank=True)
-	Gravitational_Force=models.CharField(max_length=350,blank=True)
-	mass=models.CharField(max_length=350,blank=True)
-	time=models.CharField(max_length=350,blank=True)
-	distance_travelled=models.CharField(max_length=350,unique=True)
-	speed=models.CharField(max_length=350,blank=True)
+	Gravitational_Force=models.IntegerField(max_length=350,blank=True)
+	mass=models.IntegerField(max_length=350,blank=True)
+	time=models.IntegerField(max_length=350,blank=True)
+	distance_travelled=models.IntegerField(max_length=350,unique=True)
+	speed=models.IntegerField(max_length=350,blank=True)
 	
