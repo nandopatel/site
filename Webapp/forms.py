a9885c679876cbf8c76ed6738e13c1625bbd0ef1
nandopatel <nandopatel2001@yahoo.co.uk>
2017-03-27 21:34:33 +0100

    test

--- Webapp/forms.py
+++ Webapp/forms.py
@@ -2,9 +2,8 @@ from django import forms
 
 from django.forms import ModelForm
 from .models import speedandweight
-"""
+
 class speedandweightForm(forms.ModelForm):
 	class Meta:
 		model = speedandweight
 		fields = '__all__'
-"""
