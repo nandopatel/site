7b3e3aff4b95fdd51447dae8a80e6de2efdc651a
nandopatel <nandopatel2001@yahoo.co.uk>
2017-03-27 21:30:44 +0100

    test

--- Webapp/forms.py
+++ Webapp/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 from django.forms import ModelForm
 from .models import speedandweight
+"""
 class speedandweightForm(forms.ModelForm):
 	class Meta:
 		model = speedandweight
 		fields = '__all__'
+"""
--- Webapp/models.py
+++ Webapp/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 # Create your models here.
+"""
 class speedandweight(models.Model):
 	comments=models.IntegerField(blank=True)
 	weight=models.IntegerField(blank=True)
-	Gravitational_Force=models.FloatField(max_length=350,blank=True)
+	#Gravitational_Force=models.FloatField(max_length=350,blank=True)
 	mass=models.IntegerField(max_length=350,blank=True)
 	time=models.IntegerField(max_length=350,blank=True)
 	distance_travelled=models.IntegerField(max_length=350,unique=True)
 	speed=models.IntegerField(max_length=350,blank=True)
-
+"""
 
 class pivx(models.Model):
 	price=models.FloatField()
